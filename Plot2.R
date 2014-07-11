78ba4b6567d0dae3d4844908ccb238b3e6f26212
Borja Santos Zorrozúa <bsantos001@hotmail.com>
2014-07-11 11:45:04 +0200

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,18 @@
+# Reading the data:
+database <- read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings="?")                                                                                              
+Date <- paste(database$Date,database$Time)
+date <- strptime(Date,"%d/%m/%Y %H:%M:%S")
+date2 <- as.Date(date)
+names(database)
+
+# Only the observations fo 2007-02-01 and 2007-02-02.
+database2 <- database[with(database, Date=="1/2/2007"|Date=="2/2/2007"),]
+Date2 <- paste(database2$Date,database2$Time)
+date2 <- strptime(Date2,"%d/%m/%Y %H:%M:%S")
+
+# Plot 2:
+with(database2,plot(date2,database2$Global_active_power,type="l",col="blue",ylab="Global Active Power (kilowatts)",xlab=""))
+
+# Save as PNG File:
+dev.copy(png,file = "Plot2.png")
+dev.off()
